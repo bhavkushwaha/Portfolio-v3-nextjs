093008bf5b8ed3fed94ef313cafde6d6400fdf9f
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-12-19 19:04:17 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
+   <head>
+{/* Google Analytics Script */}
+{/* Google tag (gtag.js) */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6KV5M71LL2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6KV5M71LL2');
+</script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
