d7ae4485aa8c81ca290aa5fb5e887cecfe0541ce
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-12-19 19:12:24 +0530

    layout.js Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,17 +12,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
-   <head>
-{/* Google Analytics Script */}
-{/* Google tag (gtag.js) */}
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6KV5M71LL2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-6KV5M71LL2');
-</script>
+  <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6KV5M71LL2"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6KV5M71LL2');
+          `}
+        </Script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
